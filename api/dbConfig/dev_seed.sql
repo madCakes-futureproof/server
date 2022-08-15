1d9ea08ba773731be9912ca7a27cd11f854e7a42
madCakes <jason.pbrooks@hotmail.com>
2022-08-15 13:02:50 +0100

    add seed data

---
+++ api/dbConfig/dev_seed.sql
@@ -0,0 +1,46 @@
+INSERT INTO user (username, password) 
+VALUES
+(
+    'Molly234',
+    'password1'
+),
+(
+    'captainFantastic',
+    'password12'
+),
+(
+    'Joe_User',
+    'password3'
+),
+(
+    'SteveO555',
+    'password4'
+);
+
+
+INSERT INTO habit (name, repetitions, frequency, completed) 
+VALUES
+(
+    'Gym',
+    5,
+    'Weekly',
+    true
+),
+(
+    'Drink Water 2 litres',
+    1,
+    'Daily',
+    false
+),
+(
+    'Sleep 8 hours',
+    1,
+    'Daily',
+    false
+),
+(
+    'Meditation',
+    2,
+    'Daily',
+    true
+);
