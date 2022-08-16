5569b15b1f06aff0d3dcf8d4035b8a1759660845
madCakes <jason.pbrooks@hotmail.com>
2022-08-16 13:04:27 +0100

    add static methods register,showOne

--- api/models/user.js
+++ api/models/user.js
@@ -1,38 +1,72 @@
-const db = require('../dbConfig/init');
+const db = require("../dbConfig/init");
 
 class User {
+  constructor(data) {
+    this.id = data.id;
+    this.username = data.username;
+    this.password = data.password;
+  }
 
-    constructor(data) {
-        this.id = data.id;
-        this.username = data.username;
-        this.password = data.password;
-    }
+  static getAll() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await db.query(`SELECT * FROM userAccount;`);
+        const users = result.rows.map((u) => new User(u));
+        resolve(users);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 
-    static getAll () {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const result = await db.query(`SELECT * FROM userAccount;`);
-                const users = result.rows.map(u => new User(u));
-                resolve(users);
-            } catch (err) {
-                reject(err);
-            }
-        })
-    }
+  static getOneByUsername(username) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userData = await db.query(
+          `SELECT * FROM userAccount WHERE username = $1;`,
+          [username]
+        );
+        const user = new User(userData.rows[0]);
+        resolve(user);
+      } catch (err) {
+        console.log(err);
+        reject("Unable to locate user.");
+      }
+    });
+  }
 
-    static getOneByUsername (username) {
-        return new Promise (async (resolve, reject) => {
-            try {
-                const userData = await db.query(`SELECT * FROM userAccount WHERE username = $1;`, [ username ]);
-                const user = new User(userData.rows[0]);
-                resolve (user);
-            } catch (err) {
-                console.log(err);
-                reject('Unable to locate user.');
-            }
-        })
-    }
+  static register(data) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { username, password } = data;
 
+        const newUser = await db.query(
+          `INSERT INTO userAccount (username, password) VALUES ($1, $2) RETURNING *;`,
+          [username, password]
+        );
+        let user = new User(newUser.rows[0]);
+        resolve(user);
+      } catch (err) {
+        console.log(err);
+        reject("Unable to register!");
+      }
+    });
+  }
+  // showOne takes in the id, so we need to change this function and potentially the controller too. My bad 💥
+  static showOne(username) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userData = await db.query(
+          `SELECT * FROM userAccount WHERE username = $1;`,
+          [username]
+        );
+        const user = userData.rows[0];
+        resolve(user);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 }
 
 module.exports = User;
