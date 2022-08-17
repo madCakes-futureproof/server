92eb3ad8609c4f17746edab61a12338b6ba4d173
madCakes <jason.pbrooks@hotmail.com>
2022-08-17 13:04:36 +0100

    update column user_id

--- db/migrations/2_setup_seed.sql
+++ db/migrations/2_setup_seed.sql
@@ -18,33 +18,33 @@ VALUES
 );
 
 
-INSERT INTO habit (name, repetitions, frequency, completed, streak) 
-VALUES
-(
-    'Gym',
-    5,
-    'Weekly',
-    true,
-    3
-),
-(
-    'Drink Water 2 litres',
-    1,
-    'Daily',
-    false,
-    3
-),
-(
-    'Sleep 8 hours',
-    1,
-    'Daily',
-    false,
-    3
-),
-(
-    'Meditation',
-    2,
-    'Daily',
-    true,
-    3
-);
+-- INSERT INTO habit (name, repetitions, frequency, completed, streak) 
+-- VALUES
+-- (
+--     'Gym',
+--     5,
+--     'Weekly',
+--     true,
+--     3
+-- ),
+-- (
+--     'Drink Water 2 litres',
+--     1,
+--     'Daily',
+--     false,
+--     3
+-- ),
+-- (
+--     'Sleep 8 hours',
+--     1,
+--     'Daily',
+--     false,
+--     3
+-- ),
+-- (
+--     'Meditation',
+--     2,
+--     'Daily',
+--     true,
+--     3
+-- );
