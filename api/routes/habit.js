b81fffc05706dd228a660fc8c8a11a5cf8ab6ea6
codenameStarmunch <cold_storage@icloud.com>
2022-08-18 23:48:46 +0100

    Added routes to utilise verifyToken

--- api/routes/habit.js
+++ api/routes/habit.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const habitController = require("../controllers/habit");
+// const verifyToken = require('../middleware/verifyToken');
 
 const habitRouter = Router();
 
+// habitRouter.use(verifyToken);
 habitRouter.get("/", habitController.index);
 habitRouter.get("/:id", habitController.showOne);
 habitRouter.post("/", habitController.createHabit);
